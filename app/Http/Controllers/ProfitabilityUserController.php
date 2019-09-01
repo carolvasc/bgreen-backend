4dd85c65d51f236fd9893a520a26d7bddf28150f
Juliovaz <julio.oliveiravaz0@gmail.com>
2019-09-01 13:33:04 -0300

    ajustado qtd de Invest

--- app/Http/Controllers/ProfitabilityUserController.php
+++ app/Http/Controllers/ProfitabilityUserController.php
@@ -12,7 +12,7 @@ class ProfitabilityUserController extends Controller
 
     public function index($id){
 
-        $profitability_user = DB::table('table_user_profitability')->where('id_users', 1)->where('id_investimento', $id)->get();
+        $profitability_user = DB::table('table_user_profitability')->join('table_user_investments', 'table_user_profitability.id_investimento', '=', 'table_user_investments.id')->join('users', 'table_user_profitability.id_users','=', 'users.id')->select('table_user_profitability.*', 'table_user_investments.*', 'users.*')-> get();
 
         if(count($profitability_user) > 0){
             return response()->json(
