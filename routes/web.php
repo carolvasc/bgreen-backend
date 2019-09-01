a2fdc6f2118cc64a8e2149601a9b85c8efe6d1c8
Carol Vasconcelos <carol.vasconcelos080@gmail.com>
2019-09-01 07:13:44 -0300

    open cors

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@
 
 Route::prefix('api')->group(function(){
     // users
-    Route::get('users', 'UserController@index');
-    // investments
-    Route::get('investiments', 'InvestmentsController@index');
-    Route::get('investments-users', 'InvestmentsUsersController@index'); // passar id via request
     Route::middleware(['cors'])->group(function(){
+        Route::get('users', 'UserController@index');
+        // investments
+        Route::get('investiments', 'InvestmentsController@index');
+        Route::get('investments-users', 'InvestmentsUsersController@index'); // passar id via request
         Route::post('investments-users-store', 'InvestmentsUsersController@store');
+        // rentability
+        Route::get('profitability-user', 'ProfitabilityUserController@index'); // passar id via request
     });
-    // rentability
-    Route::get('profitability-user', 'ProfitabilityUserController@index'); // passar id via request
 });
